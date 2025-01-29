47f2f014c4f27f6ea2d4850d30fdb60d1b53f0e0
peyro <peyrovskaaa@gmail.com>
2025-01-29 19:53:40 +0100

    initial commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from config import API_BOT_TOKEN
+from config import SUPPORT_TELEGRAM_BOT_TOKEN
 
 
-bot = Bot(token=API_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=SUPPORT_TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_BOT_TOKEN = os.getenv("API_BOT_TOKEN")
+SUPPORT_TELEGRAM_BOT_TOKEN = os.getenv("API_BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_LOGIN_KEY = os.getenv("SECRET_LOGIN_KEY")
 BASE_SERVER_URL = "http://127.0.0.1:8000/"
