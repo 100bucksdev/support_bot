25bd1050d6508ce3169f02cf521d2141b46af52b
peyro <peyrovskaaa@gmail.com>
2025-07-25 15:05:35 +0200

    Reorder router inclusions for improved handler organization

--- server_requests.py
+++ server_requests.py
@@ -6,9 +6,13 @@ from config import BASE_SERVER_URL
 
 async def make_request(data, message):
     url = f'{BASE_SERVER_URL}api/v1/chatbot/chat/operator/send-message/'
+    try:
 
-    response = requests.post(url, json=data)
+        response = requests.post(url, json=data)
 
-    response_json = response.json()
+        response_json = response.json()
+    except Exception as e:
+        print(e)
+        return
     if response_json.get('message') == 'chat_not_exist':
         await message.answer('❌ The message was not delivered because the chat does not exist', reply_markup=ReplyKeyboardRemove())
